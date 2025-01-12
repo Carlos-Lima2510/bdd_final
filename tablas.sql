80d5a3466738448dfb1901405999a78f88a59133
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-01-12 15:35:42 +0100

    feature/ciudad

--- tablas.sql
+++ tablas.sql
@@ -70,17 +70,18 @@ CREATE TABLE bases (
     base_id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100),
     codigo VARCHAR(10),
+    ciudad VARCHAR(100),
     UNIQUE (codigo)
 ) ENGINE=InnoDB
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_general_ci;
 
-INSERT INTO bases (nombre, codigo) VALUES
-('Base Internacional JFK', 'JFK'),
-('Base Internacional LAX', 'LAX'),
-('Base Internacional ORD', 'ORD'),
-('Base Internacional ATL', 'ATL'),
-('Base Internacional CDG', 'CDG');
+INSERT INTO bases (nombre, codigo, ciudad) VALUES
+('Base Internacional JFK', 'JFK', 'Nueva York'),
+('Base Internacional LAX', 'LAX', 'Los Ángeles'),
+('Base Internacional ORD', 'ORD', 'Chicago'),
+('Base Internacional ATL', 'ATL', 'Atlanta'),
+('Base Internacional CDG', 'CDG', 'París'),
 
 
 
