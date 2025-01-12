81bb4ab5e0ead1d9086dd7fdbb1ec576745e0d9b
Carlos-Lima2510 <carlos251005.alvarado@gmail.com>
2025-01-12 14:15:59 +0100

    Creación de entorno de trabajo

---
+++ consultas.sql
@@ -0,0 +1,64 @@
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_alejandro_juarez` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_alejandro_juarez`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_kelvia_neves` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_kelvia_neves`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_carlos_alvarado` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_carlos_alvarado`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_manuel_munoz` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_manuel_munoz`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+DROP PROCEDURE IF EXISTS `consultas_miguel_rivas` //
+
+CREATE DEFINER=`root`@`%` PROCEDURE `consultas_miguel_rivas`()
+BEGIN
+
+-- Consultas --
+
+END //
+
+DELIMITER ;
