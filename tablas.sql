493bc8b9e64f5527627afb66039ef55bccce4db3
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-01-12 15:17:48 +0100

    feature/create table bases

--- tablas.sql
+++ tablas.sql
@@ -64,9 +64,16 @@
 
 
 -- Tabla de Miguel --
+DROP TABLE IF EXISTS bases;
 
-
-
+CREATE TABLE bases (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100),
+    codigo VARCHAR(10),
+    UNIQUE (codigo)
+) ENGINE=InnoDB
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
 
 
