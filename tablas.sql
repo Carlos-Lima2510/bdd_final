89dadf25feb928358ac47a8cb701bd633d7e4a63
Carlos-Lima2510 <carlos251005.alvarado@gmail.com>
2025-01-12 15:25:47 +0100

    insercion de tabla de carlos

--- tablas.sql
+++ tablas.sql
@@ -79,3 +79,16 @@
 
 
 -- Tabla de Carlos --
+CREATE TABLE IF NOT EXISTS vuelo (
+    vuelo_id INT PRIMARY KEY AUTO_INCREMENT,
+    codigo_de_vuelo CHAR(7) NOT NULL,
+    origen VARCHAR(150) NOT NULL,
+    destino VARCHAR(200) NOT NULL,
+    hora_de_vuelo TIME NOT NULL,
+    avion_id int NOT NULL,
+    codigo_piloto int NOT NUll,
+    codigo_miembro int NOT NULL
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+INSERT INTO vuelo (codigo_de_vuelo, origen, destino, hora_de_vuelo, avion_id, codigo_piloto, codigo_miembro)
+VALUES ('BA-1034', "Palma", "Alicante", "13:50:00", 1, 1,1);
