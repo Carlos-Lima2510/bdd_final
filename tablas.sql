f97e0f1860b92f5eb4679ea0ab9bfccbd38597dd
Alejandrojuarez0105 <alejandrojuarez01050812@gmail.com>
2025-01-12 15:30:40 +0100

    Pilotos

--- tablas.sql
+++ tablas.sql
@@ -1,8 +1,13 @@
 -- Tabla de Alejandro -- 
+CREATE TABLE IF NOT EXISTS `Pilotos` (
+    `codigo_piloto` INT AUTO_INCREMENT PRIMARY KEY,
+    `nombre` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
+    `horas_vuelo` INT,
+    `base_id` INT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 
-
-
+INSERT INTO `Pilotos` (`nombre`, `horas_vuelo`, `base_id`) VALUES ('Alejandro Juarez', 1500, 1), ('Miguel Rivas', 5000, 1), ('Carlos Lima', 1000, 1), ('Manuel Muñoz', 2000, 3), ('Kelvia Neves', 2000, 5), ('Fulano', 4000, 6);
 
 
 
