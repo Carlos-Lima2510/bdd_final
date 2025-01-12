c8c805dbd04fe7a2aae7a3fa174ae890ca20e267
KelviaBarros05 <barroskelvia122@gmail.com>
2025-01-12 15:33:12 +0100

    feat/tabla avion

--- tablas.sql
+++ tablas.sql
@@ -21,26 +21,16 @@
 
 -- Tabla de Kelvia --
 
+CREATE TABLE avion (
+    avion_id INT AUTO_INCREMENT PRIMARY KEY,
+    codigo VARCHAR(50),
+    base_id INT
+) ENGINE=InnoDB CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+INSERT INTO avion (codigo, base_id) VALUES
+('BOEING-747', 1),
+('AIRBUS-A320', 2),
+('EMBRAER-190', 3);
 
 -- Tabla de Manuel ---
 
