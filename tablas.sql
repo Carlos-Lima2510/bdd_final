28c3a2d2d1b173013e43f66de5c4235d65dca7c4
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-01-12 15:57:55 +0100

    feature/consultas

--- tablas.sql
+++ tablas.sql
@@ -82,6 +82,11 @@ INSERT INTO bases (nombre, codigo, ciudad) VALUES
 ('Base Internacional ORD', 'ORD', 'Chicago'),
 ('Base Internacional ATL', 'ATL', 'Atlanta'),
 ('Base Internacional CDG', 'CDG', 'París'),
+('Base Internacional MAD', 'MAD', 'Madrid'),
+('Base Internacional SFO', 'SFO', 'San Francisco'),
+('Base Internacional SYD', 'SYD', 'Sídney'),
+('Base Internacional DXB', 'DXB', 'Dubái'),
+('Base Internacional NRT', 'NRT', 'Tokio');
 
 
 
